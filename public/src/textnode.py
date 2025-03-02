27d0d40e13e204ed83b7882d4690b51368235e47
Veerden <keano.denton@gmail.com>
2025-03-02 01:11:36 -0500

    second base update

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -14,3 +14,17 @@ class TextNode:
     self.text = text
     self.text_type = text_type
     self.url = url
+
+  def __eq__(self, other_node):
+    if not isinstance(other_node, TextNode):
+      return False
+    
+    return(
+      self.text == other_node.text and
+      self.text_type == other_node.text_type and 
+      self.url == other_node.url
+    )
+
+
+  def __repr__(self):
+    pass
