56d1f475ced82b4a84813d691e4a40a760a12a90
Veerden <keano.denton@gmail.com>
2025-03-02 22:41:12 -0500

    Test files and cases.  With new HTML child class beginning

---
+++ src/htmlnode.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+class HTMLNode:
+  def __init__(self,tag=None, value=None, children=None, props=None):
+    self.tag = tag
+    self.value = value
+    self.children = children
+    self.props = props
+    
+  def to_html(self):
+    raise NotImplementedError("to_html method not implemented")
+  
+  def props_to_html(self):
+    if not self.props:
+      return ""
+    props_str = ""
+    for key, value in self.props.items():
+      props_str += f' {key}="{value}"'
+    return props_str
+  
+  def __repr__(self):
+    return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+  
+class LeafNode(HTMLNode):
+  def __init__(self, )
--- src/textnode.py
+++ src/textnode.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 class TextType(Enum):
 
-  NORMAL_TEXT = "normal text"
-  BOLD_TEXT = "bold text"
-  ITALIC_TEXT = "italic text"
-  CODE_TEXT = "code text"
+  NORMAL = "normal text"
+  BOLD = "bold text"
+  ITALIC = "italic text"
+  CODE = "code text"
   LINKS = "links"
   IMAGES = "images"
 
