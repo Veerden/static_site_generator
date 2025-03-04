e0e29b23cb05f743b48ec021c40ef8f1bb66ed30
Veerden <keano.denton@gmail.com>
2025-03-03 22:23:37 -0500

    Monday March 3, 2025: TextNode to HTMLNode method creation without test.

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -22,4 +22,55 @@ class HTMLNode:
     return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
   
 class LeafNode(HTMLNode):
-  def __init__(self, )
+  def __init__(self, tag, value, props=None):
+    super().__init__(tag, value, children=None, props=props)
+    self.value = value
+    self.tag = tag
+    self.props = props
+
+  def to_html(self):
+    if self.value == None:
+      raise ValueError("Leaf nodes must have a value")
+    if self.tag is None:
+      return self.value
+    html = "<" + self.tag
+
+    if self.props:
+      for prop_name, prop_value in self.props.items():
+        html += f' {prop_name}="{prop_value}"'
+    html += ">" + self.value + "</" + self.tag + ">"
+
+    return html
+  
+class ParentNode(HTMLNode):
+  def __init__(self, tag, children, props=None):
+    super().__init__(tag, children, props=props)
+    self.tag = tag
+    self.children = children
+    self.props = props
+
+  def to_html(self):
+    if self.tag is None:
+        raise ValueError("Parent nodes must have a tag")
+    if self.children is None:
+        raise ValueError("Parent nodes must have children")
+    
+    # Start with the opening tag
+    html = f"<{self.tag}"
+    
+    # Add any properties if they exist
+    if self.props:
+        props_html = self.props_to_html()
+        html += props_html
+    
+    # Close the opening tag
+    html += ">"
+    
+    # Add all children's HTML
+    for child in self.children:
+        html += child.to_html()
+    
+    # Add the closing tag
+    html += f"</{self.tag}>"
+    
+    return html
